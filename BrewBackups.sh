a09cdf0c7deda0efef57488df135b00df3ab61f4
Travis Williamson <travis.m.williamson@gmail.com>
2025-03-16 21:13:02 -0500

    Clean up ~, add OS specific packages, add sourcing

--- BrewBackups.sh
+++
@@ -1,6 +0,0 @@
-BREWFILE_BACKUP_LOCATION=~/Documents/Brewfiles
-DATE=`date +%d-%m-%Y`
-
-cd /tmp
-brew bundle dump
-mv Brewfile "$BREWFILE_BACKUP_LOCATION/Brewfile-$DATE"
--- scripts/packages/install.sh
+++ scripts/packages/install.sh
@@ -3,10 +3,21 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
+source ~/scripts/os.source
+
 COMMENT=\#*
 
-find * -name "*.list" | while read fn; do
+# TODO: Bootstrap package managers by OS
+# Windows: choco
+# macos: brew
+# macos: Something for brew taps, must run before brew, currently need
+# brew tap microsoft/git
+
+# Common packages in this directory
+# OS specific packages in child directory of that name
+{ find * -maxdepth 0 -name "*.list"; find $(os) -name "*.list"; } | while read fn; do
     cmd="${fn%.*}"
+    cmd="${cmd#*/}"
     while read package; do
         if [[ $package == $COMMENT ]];
         then continue
