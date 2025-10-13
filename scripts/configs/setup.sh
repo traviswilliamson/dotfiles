2acd241ae05bd4553da7d607fa3a2d7eca65a825
Travis Williamson <travis.m.williamson@gmail.com>
2025-10-13 15:34:10 -0500

    Add VSCode settings, symlink

--- scripts/configs/setup.sh
+++ scripts/configs/setup.sh
@@ -7,13 +7,31 @@ DIR=$(dirname "$0")
 pushd "$DIR" > /dev/null
 
 # git
-filetolink="$HOME/scripts/configs/git/.gitconfig.$(os)"
-info "Symlinking .gitconfig.thisos to $filetolink"
-ln -sf "$filetolink" "$HOME/.gitconfig.thisos"
+gitfiletolink="$HOME/scripts/configs/git/.gitconfig.$(os)"
+info "Symlinking .gitconfig.thisos to $gitfiletolink"
+ln -sf "$gitfiletolink" "$HOME/.gitconfig.thisos"
 
 # os
 info "Configuring OS settings"
 bash "./os/$(os).sh"
+
+# VSCode
+vscodepath=""
+case $(os) in
+    "linux")
+        vscodepath="$HOME/.config/Code/User"
+        ;;
+    "windows")
+        vscodepath="$APPDATA/Code/User"
+        ;;
+    "macos")
+        vscodepath="$HOME/Library/Application Support/Code/User"
+        ;;
+esac
+info "Symlinking VSCode settings"
+mkdir -p "$vscodepath"
+ln -Pf "$HOME/scripts/configs/vscode/keybindings.json" "$vscodepath/keybindings.json"
+ln -Pf "$HOME/scripts/configs/vscode/settings.json" "$vscodepath/settings.json"
 
 # Something for windows terminal profiles?
 
